b72ca994c60c107d34953eaa6a850f56061b2b43
Hamza-ABAKI <hamza.abaki@gmail.com>
2021-05-07 23:48:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import imageInSrc from "./imageInSrc.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ border: "solid 2px black", maxWidth: "100vw" }}>
+        <h1
+          className="title red"
+          style={{
+            color: "red",
+            margin: "5px 10px",
+            padding: "5px 5px",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Hey !!! My name is Hamza
+        </h1>
+        {/* <br /> */}
+        <div className="image">
+          <div className="source">
+            <img src={imageInSrc} alt="imageInSrc" />
+            <h3>This image from src folder</h3>
+          </div>
+          {/* <br /> */}
+          <div className="public">
+            <img src="/imageInPublic.jpg" alt="imageInPublic" />
+            <h3>This image from public folder</h3>
+          </div>
+        </div>
+      </div>
+      <div className="vid">
+        <h4>Al-HATABA</h4>
+        <iframe
+          title="Al-Hataba"
+          width={520}
+          height={360}
+          src="https://www.youtube.com/embed/BiLHVmphUwA"
+        />
+      </div>
     </div>
   );
 }
